aacb3ac18ec8031ccbad33be4ea65c9215953416
Cristian de Mellos <cristian.ms@hotmail.com>
2019-05-22 10:49:49 -0300

    Iniciando com banco de dados postgres, utilizando knex

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
 const express = require('express')
 const app = express()
 
-
-app.get('/', (req, res) => {
-    res.status(200).send('<h1>Meu backend!</h1>')
-})
-
 app.listen(3000, () => {
     console.log('Backend executando...')
 })
